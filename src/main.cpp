0b5bac9af126c0862f0beff619bc78f2f20dfcd7
pkb-pmj <103094889+pkb-pmj@users.noreply.github.com>
2022-04-10 23:01:21 +0200

    refactor(popCount): change 64-bit to 32-bit
    BREAKING CHANGE

--- src/main.cpp
+++ src/main.cpp
@@ -87,14 +87,13 @@ const int LETTERS_LENGTH = sizeof(letters) / sizeof(letters[0]);
 unsigned long bitfield = 0;
 
 // Zwraca ilość bitów równych 1 w podanej zmiennej.
-int popCount(uint64_t x) {
-  x -= (x >> 1) & 0x5555555555555555;             //put count of each 2 bits into those 2 bits
-  x = (x & 0x3333333333333333) + ((x >> 2) & 0x3333333333333333); //put count of each 4 bits into those 4 bits
-  x = (x + (x >> 4)) & 0x0f0f0f0f0f0f0f0f;        //put count of each 8 bits into those 8 bits
-  x += x >>  8;  //put count of each 16 bits into their lowest 8 bits
-  x += x >> 16;  //put count of each 32 bits into their lowest 8 bits
-  x += x >> 32;  //put count of each 64 bits into their lowest 8 bits
-  return x & 0x7f;
+int popCount(uint32_t x) {
+  x = x - ((x >> 1) & 0x55555555);
+  x = (x & 0x33333333) + ((x >> 2) & 0x33333333);
+  x = (x + (x >> 4)) & 0x0F0F0F0F;
+  x = x + (x >> 8);
+  x = x + (x >> 16);
+  return x & 0x0000003F;
 }
 
 // Zamienia bitfield na string złożony z 0 i 1.
